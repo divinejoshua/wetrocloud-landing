00cfc0c9fb76c8d5978a91f52302c84ee072c536
divinejoshua <divineuche399@gmail.com>
2025-01-31 11:26:33 +0000

    Added suite section and integration sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,20 @@ import geminiLogo from '@/app/assets/img/gemini-logo.png'
 import ragIcon from '@/app/assets/img/rag-icon.png'
 import textGenerationIcon from '@/app/assets/img/text-generation-icon.png'
 import imageGenerationIcon from '@/app/assets/img/image-generation-icon.png'
+import audioIcon from '@/app/assets/img/audio-icon.png'
+import videoTranscribeIcon from '@/app/assets/img/video-transcriber.png'
+import textExtractIcon from '@/app/assets/img/text-extract-icon.png'
+import slackLogo from '@/app/assets/img/slack-logo.png'
+import notionLogo from '@/app/assets/img/notion-logo.png'
+import gmailLogo from '@/app/assets/img/gmail-logo.png'
+import whatsappLogo from '@/app/assets/img/whatsapp-logo.png'
+import ms365Logo from '@/app/assets/img/microsoft-logo.png'
+import stripeLogo from '@/app/assets/img/stripe-logo.png'
+import adobeLogo from '@/app/assets/img/adobe-logo.png'
+import shopifyLogo from '@/app/assets/img/shopify-logo.png'
+import discordLogo from '@/app/assets/img/discord-logo.png'
+import githubLogo from '@/app/assets/img/github-logo.png'
+
 
 
 export default function Home() {
@@ -28,7 +42,7 @@ export default function Home() {
             </div>
 
             {/* Hero image */}
-            <Image src={heroImage} alt="Wetrocloud" className='hero-image mt-10'/>
+            <Image src={heroImage} alt="Wetrocloud" className='hero-image mt-10 rounded-lg'/>
 
             <p className="text-3xl mt-16 font-medium"><span className="text-3xl font-medium text-default">20x</span> faster than traditional AI models with the use of inference</p>
 
@@ -43,7 +57,7 @@ export default function Home() {
 
             {/* AI suite */}
             <section className="mt-20">
-              <h4 className="text-4xl font-medium text-center">Custom AI Software suite</h4>
+              <h3 className="text-4xl font-medium text-center">Custom AI Software suite</h3>
 
               <p className="mt-7 text-gray-500 text-center">Wetrocloud provides various custom tools that you can use to automate your workflow</p>
 
@@ -52,78 +66,100 @@ export default function Home() {
                   <div className="container mx-auto">
 
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-lg suite-card flex flex-col justify-between">
                         <div>
                           <Image src={ragIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
                           <h3 className="text-lg font-medium text-gray-800 mb-4">RAG and AI Agent APIs</h3>
-                          <p className="text-gray-600">
-                          Easy to integrate APIs to build your AI Infrastructure.
+                          <p className="text-gray-600 mt-6">
+                          Powerful and easy-to-integrate RAG and AI Agent APIs to enhance your AI infrastructure. Seamlessly connect, retrieve, and generate intelligent responses for advanced applications.
                           </p>
                         </div>
                         <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
                         </div>
 
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-lg suite-card flex flex-col justify-between">
                         <div>
                           <Image src={textGenerationIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
                           <h3 className="text-lg font-medium text-gray-800 mb-4">Text to Audio</h3>
                           <p className="text-gray-600">
-                          Convert text to audio seamlessly.
+                          Generate natural-sounding audio from text with seamless Text-to-Audio APIs. Perfect for voice assistants, audiobooks, and interactive applications.
                           </p>
                         </div>
                         <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
                         </div>
 
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-lg suite-card flex flex-col justify-between">
                         <div>
                           <Image src={imageGenerationIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
                           <h3 className="text-lg font-medium text-gray-800 mb-4">Video Transcriber</h3>
                           <p className="text-gray-600">
-                          Transcribe videos with high accuracy.
+                          Accurately convert video speech to text with advanced Video Transcriber APIs. Ideal for captions, subtitles, and content indexing.
                           </p>
                         </div>
                         <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
                         </div>
 
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-sm suite-card flex flex-col justify-between">
                         <div>
-                          <Image src={ragIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
-                          <h3 className="text-lg font-medium text-gray-800 mb-4">RAG and AI Agent APIs</h3>
-                          <p className="text-gray-600">
-                          Easy to integrate APIs to build your AI Infrastructure.
-                          </p>
-                        </div>
-                        <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
-                        </div>
-
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
-                        <div>
-                          <Image src={textGenerationIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
+                          <Image src={audioIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
                           <h3 className="text-lg font-medium text-gray-800 mb-4">Text to Audio</h3>
                           <p className="text-gray-600">
-                          Convert text to audio seamlessly.
+                          Convert text into natural, lifelike speech with Text-to-Audio APIs. Perfect for voice assistants, podcasts, and accessibility solutions.
                           </p>
                         </div>
                         <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
                         </div>
 
-                        <div className="border border-gray-200 p-6 rounded-sm h-80 flex flex-col justify-between">
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-sm suite-card flex flex-col justify-between">
                         <div>
-                          <Image src={imageGenerationIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
-                          <h3 className="text-lg font-medium text-gray-800 mb-4">Video Transcriber</h3>
+                          <Image src={videoTranscribeIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
+                          <h3 className="text-lg font-medium text-gray-800 mb-4">Video transcriber</h3>
                           <p className="text-gray-600">
-                          Transcribe videos with high accuracy.
+                          Effortlessly transcribe video audio into accurate text with Video Transcriber APIs. Ideal for captions, subtitles, and content analysis.
                           </p>
                         </div>
                         <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
                         </div>
 
-                    
-
+                        <div className="border border-gray-200 px-6 pt-6 pb-10 rounded-sm suite-card flex flex-col justify-between">
+                        <div>
+                          <Image src={textExtractIcon} alt="Wetrocloud" className="suite-icon mt-3 mb-7"/>
+                          <h3 className="text-lg font-medium text-gray-800 mb-4">Text and Data extration</h3>
+                          <p className="text-gray-600">
+                          Extract valuable insights from text and data with powerful extraction APIs. Ideal for document processing, automation, and analytics.
+                          </p>
+                        </div>
+                        <button className="bg-black text-white py-2 px-6 rounded-sm hover:bg-blue-950 w-fit">Learn more</button>
+                        </div>
 
                     </div>
                   </div>
                 </div>
+            </section>
+
+
+            {/* Integrations */}
+            <section className="mt-20">
+              <h3 className="text-4xl font-medium text-center">With over 50+ integrations</h3>
+              <p className="mt-7 text-gray-500 text-center">Wetrocloud Apps can be integrated into more than 100 softwares and APIs</p>
+            
+              {/* Integrations logos */}
+              <div className="flex space-x-16 mt-16 mx-auto w-full justify-center">
+                <div><Image src={slackLogo} alt="Wetrocloud" className="model-logo"/></div>
+                <div><Image src={notionLogo} alt="Wetrocloud" className="model-logo mt-1"/></div>
+                <div><Image src={gmailLogo} alt="Wetrocloud" className="model-logo mt-2"/></div>
+                <div><Image src={whatsappLogo} alt="Wetrocloud" className="model-logo mt-2"/></div>
+                <div><Image src={ms365Logo} alt="Wetrocloud" className="model-logo mt-2"/></div>
+              </div>
+
+              <div className="flex space-x-16 mt-10 mx-auto w-full justify-center">
+                <div><Image src={stripeLogo} alt="Wetrocloud" className="model-logo"/></div>
+                <div><Image src={adobeLogo} alt="Wetrocloud" className="model-logo mt-3"/></div>
+                <div><Image src={shopifyLogo} alt="Wetrocloud" className="model-logo mt-2"/></div>
+                <div><Image src={discordLogo} alt="Wetrocloud" className="model-logo mt-2"/></div>
+                <div><Image src={githubLogo} alt="Wetrocloud" className="model-logo"/></div>
+              </div>
+
             </section>
 
           </main>
