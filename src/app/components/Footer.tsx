0bbdc12df3b15a6b1dd9bc809d885ce762d78984
divinejoshua <divineuche399@gmail.com>
2025-05-19 13:29:04 +0100

    Completed footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,11 @@
 import linkedInLogo from '@/app/assets/img/linkedin-social-icon.png'
 import emailLogo from '@/app/assets/img/email-social-icon.png'
 import youtubeLogo from '@/app/assets/img/youtube-logo.png'
+import headerLogo from '@/app/assets/img/header-logo.png'
 import discordLogo from '@/app/assets/img/discord-social-icon.png'
+import twitterLogo from '@/app/assets/img/twitter-social-icon.png'
 import Image from 'next/image'
-import { API_REFERENCES_URL, BLOG_URL, BOOK_CALL_URL, CONSOLE_URL, DISCORD_URL, DOCS_RAG_API_URL, DOCS_TEXT_GENERATION_API_URL, LINKEDIN_URL, PLAYGROUND_URL, PRIVACY_POLICY_URL, TERMS_OF_SERVICE_URL, YOUTUBE_URL } from '../constants/constants'
+import { API_REFERENCES_URL, BLOG_URL, BOOK_CALL_URL, CONSOLE_URL, DISCORD_URL, DOCS_RAG_API_URL, DOCS_TEXT_GENERATION_API_URL, LINKEDIN_URL, PRICING_URL, PRIVACY_POLICY_URL, TERMS_OF_SERVICE_URL, TWITTER_URL, YOUTUBE_URL } from '../constants/constants'
 import Link from 'next/link'
 
 export default function FooterComponent() {
@@ -60,40 +62,44 @@ export default function FooterComponent() {
             <h3 className="text-4xl font-medium text-center">Get started with Wetrocloud</h3>
             <p className="mt-7 text-gray-500 text-center max-w-xl mx-auto">Get started with Wetrocloud today and unleash the full potential of your data extraction and data‑driven RAG applications.</p>
             <div className="flex items-center justify-center space-x-4 mt-5">
-              <a href={CONSOLE_URL} target="_blank"><button className="w-full sm:w-auto bg-default text-white px-6 py-3 rounded-full hover:bg-gray-800 transition mt-3">Get started for free</button></a>
+              <a href={CONSOLE_URL} target="_blank"><button className="w-full sm:w-auto bg-default text-white px-6 py-3 rounded-full hover:bg-blue-600 transition mt-3">Get started for free</button></a>
             </div>
         </div>
 
         {/* Footer */}
-        <div className="mt-20 bg-gray-100 py-10">
+        <div className="mt-20 bg-gray-100 py-16">
 
-                <div className="max-w-6xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div className="mx-auto max-w-7xl px-3">
+                    <Image src={headerLogo} alt="Wetrocloud" className="header-logo w-8 h-8" />
+                </div>
+
+                <div className="max-w-7xl mt-10 mx-auto px-3 grid grid-cols-1 md:grid-cols-4 gap-8">
                 {/* Terms */}
                 <div>
-                <h3 className="font-bold">Terms</h3>
+                <h3 className="font-bold">Legal</h3>
                 <ul className="mt-5 space-y-3">
-                    <li><Link href={TERMS_OF_SERVICE_URL} className="hover:underline">Terms of service</Link></li>
-                    <li><Link href={PRIVACY_POLICY_URL} className="hover:underline">Privacy policy</Link></li>
+                    <li><Link href={TERMS_OF_SERVICE_URL} className="hover:underline transition">Terms of service</Link></li>
+                    <li><Link href={PRIVACY_POLICY_URL} className="hover:underline transition">Privacy policy</Link></li>
                 </ul>
                 </div>
 
                 {/* Product */}
                 <div>
-                <h3 className="font-bold">Product</h3>
+                <h3 className="font-bold">Company</h3>
                 <ul className="mt-5 space-y-3">
-                    <li><a href={BOOK_CALL_URL} target="_blank" className="hover:underline">Book a demo</a></li>
-                    <li><a href={PLAYGROUND_URL} target='_blank' className="hover:underline">Playground</a></li>
-                    <li><a href={BLOG_URL} target='_blank' className="hover:underline">Blog</a></li>
+                    <li><a href={BLOG_URL} target='_blank' className="hover:underline transition">Blog</a></li>
+                    <li><Link href={PRICING_URL}  className="hover:underline transition">Pricing</Link></li>
+                    <li><a href={BOOK_CALL_URL} target="_blank" className="hover:underline transition">Book a demo</a></li>
                 </ul>
                 </div>
 
                 {/* Developer */}
                 <div>
-                <h3 className="font-bold">Developer</h3>
+                <h3 className="font-bold">Documentation</h3>
                 <ul className="mt-5 space-y-3">
-                    <li><a href={DOCS_TEXT_GENERATION_API_URL} target='_blank' className="hover:underline">Chat APIs</a></li>
-                    <li><a href={DOCS_RAG_API_URL} target='_blank' className="hover:underline">RAG APIs</a></li>
-                    <li><a href={API_REFERENCES_URL} target='_blank' className="hover:underline">API references</a></li>
+                    <li><a href={DOCS_TEXT_GENERATION_API_URL} target='_blank' className="hover:underline transition">Data Extraction API</a></li>
+                    <li><a href={DOCS_RAG_API_URL} target='_blank' className="hover:underline transition">RAG API</a></li>
+                    <li><a href={API_REFERENCES_URL} target='_blank' className="hover:underline transition">API references</a></li>
                 </ul>
                 </div>
 
@@ -102,6 +108,7 @@ export default function FooterComponent() {
                     <h3 className="font-bold">Contact</h3>
                     <div className="flex space-x-4 mt-2">
                         <a href={LINKEDIN_URL} target='_blank' className="text-xl hover:text-gray-600"><Image src={linkedInLogo} alt="Wetrocloud" className="social-icon"/></a>
+                        <a href={TWITTER_URL} target='_blank' className="text-xl hover:text-gray-600"><Image src={twitterLogo} alt="Wetrocloud" className="social-icon"/></a>
                         <a href="mailto:hello@wetrocloud.com" target='_blank' className="text-xl hover:text-gray-600"><Image src={emailLogo} alt="Wetrocloud" className="social-icon"/></a>
                         <a href={YOUTUBE_URL} target='_blank' className="text-xl hover:text-gray-600"><Image src={youtubeLogo} alt="Wetrocloud" className="social-icon"/></a>
                         <a href={DISCORD_URL} className="text-xl hover:text-gray-600" target='_blank'><Image src={discordLogo} alt="Wetrocloud" className="social-icon"/></a>
